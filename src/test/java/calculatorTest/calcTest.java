64319b906163061a150f45eac8aad4000afd2c80
farahdilea <152026803+farahdilea@users.noreply.github.com>
2024-05-12 23:07:34 +0300

    Update calcTest.java

--- src/test/java/calculatorTest/calcTest.java
+++ src/test/java/calculatorTest/calcTest.java
@@ -5,20 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class calcTest {
-
+	
 	@Test
+	
 	void testSum() {
 		
 		calculator calc= new calculator();
 		int sumResult = calc.sum(1,3);
+		System.out.println("Starting a boring test");
 		assertEquals(4,sumResult);
+		System.out.println("Finished a boring test");
+		
 	}
 	@Test
 	void testSub() {
 		
 		calculator calc= new calculator();
 		int subResult = calc.sub(20,15);
+		System.out.println("Starting a boring test");
+		
 		assertEquals(5,subResult);
+		System.out.println("Finished a boring test");
 	}	
 	
 	@Test
@@ -26,14 +33,21 @@ class calcTest {
 		
 		calculator calc= new calculator();
 		int multResult = calc.mult(20,10);
+		System.out.println("Starting a boring test");
+		
 		assertEquals(200,multResult);
+		System.out.println("Finished a boring test");
 	}	
-
+	
+	@Test
 	void testDiv() {
 		
 		calculator calc= new calculator();
 		double divResult = calc.div(20,10);
+		System.out.println("Starting a boring test");
+		
 		assertEquals(2.0,divResult);
+		System.out.println("Finished a boring test");
 	}	
 
 
