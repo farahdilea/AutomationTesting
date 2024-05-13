6c0204cd39fe32a66247f9b39024102d69242810
farahdilea <152026803+farahdilea@users.noreply.github.com>
2024-05-13 21:30:25 +0300

    Update calcTest.java

--- src/test/java/calculatorTest/calcTest.java
+++ src/test/java/calculatorTest/calcTest.java
@@ -1,54 +1,55 @@
 package calculatorTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class calcTest {
 	
-	@Test
+	@BeforeAll
+	static void beforeAllTests() {
+		System.out.println("Started testing the great calculator app");
+	    }
 	
+	@BeforeEach
+	void printB(){
+		System.out.println("Starting a boring test");
+	    }
+	
+        @AfterEach 
+	void printA() {
+    	System.out.println("Finished a boring test");
+        }
+
+	@Test
 	void testSum() {
-		
 		calculator calc= new calculator();
 		int sumResult = calc.sum(1,3);
-		System.out.println("Starting a boring test");
-		assertEquals(4,sumResult);
-		System.out.println("Finished a boring test");
-		
-	}
+        assertEquals(4,sumResult);
+	    }
+	
 	@Test
 	void testSub() {
-		
 		calculator calc= new calculator();
 		int subResult = calc.sub(20,15);
-		System.out.println("Starting a boring test");
-		
 		assertEquals(5,subResult);
-		System.out.println("Finished a boring test");
-	}	
+		}	
 	
 	@Test
 	void testMult() {
-		
 		calculator calc= new calculator();
 		int multResult = calc.mult(20,10);
-		System.out.println("Starting a boring test");
-		
-		assertEquals(200,multResult);
-		System.out.println("Finished a boring test");
-	}	
+	    assertEquals(200,multResult);
+	    }	
 	
 	@Test
 	void testDiv() {
-		
 		calculator calc= new calculator();
 		double divResult = calc.div(20,10);
-		System.out.println("Starting a boring test");
-		
-		assertEquals(2.0,divResult);
-		System.out.println("Finished a boring test");
-	}	
+                assertEquals(2.0,divResult);
+	    }	
 
 
 }
